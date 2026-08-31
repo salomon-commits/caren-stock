e6bc0333bd4d0dececda3ac41444d0563d046198
salomon-commits <serviceclient.easylink@gmail.com>
2026-08-31 11:33:42 +0000

    Déployer les améliorations mobiles de Carène Stock

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caren-stock-shell-v6';
+const CACHE_NAME = 'caren-stock-shell-v7';
 const ENHANCEMENT_URL = './enhancements-inline.js';
 const RECEIPT_CONTROLS_URL = './receipt-preview-controls.js';
 const APP_SHELL = [
