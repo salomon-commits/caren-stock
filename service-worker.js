caef37b411bf4b9c631c14ea6a745ec86234c905
salomon-commits <serviceclient.easylink@gmail.com>
2026-09-02 14:54:52 +0000

    Déployer le compteur de sorties par produit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caren-stock-shell-v10';
+const CACHE_NAME = 'caren-stock-shell-v11';
 const ENHANCEMENT_URL = './enhancements-inline.js';
 const RECEIPT_CONTROLS_URL = './receipt-preview-controls.js';
 const APP_SHELL = [
